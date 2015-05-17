5ff7436fd1a1f540d1aad2e358e6e27dc5e1cb09
Madhusudhan Dollu <madhusudhan.dollu@gmail.com>
2015-05-17 14:30:18 +0530

    added setup sample template

--- app/views/setup/setup.blade.php
+++ app/views/setup/setup.blade.php
@@ -1,12 +1,24 @@
 @extends('setup.layout')
 
 @section('title')
-Vave Configuration
+Vave Setup
 @stop
 
 
 @section('body')
 
-    <h1 class="text-center">Welcome to Vave Setup</h1>
+<h1 class="text-center">Welcome to Vave Setup</h1>
+
+<div style="width:400px;margin:0 auto;">
+    {{ Form::open(['route'=>'home']) }}
+    {{ Form::label('Full Name') }}
+    {{ Form::text('name','',['class'=>'form-control']) }}
+    {{ Form::label('Email') }}
+    {{ Form::email('email','',['class'=>'form-control']) }}
+    {{ Form::label('Password') }}
+    {{ Form::password('password',['class'=>'form-control']) }}
+    {{ Form::submit('Setup Vave',['class'=>'btn btn-primary']) }}
+    {{ Form::close() }}
+</div>
 
 @stop
