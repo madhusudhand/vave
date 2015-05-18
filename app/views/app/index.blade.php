06e0e9bb14d6ed5426ce0caf84c7b4f537138e9d
Madhusudhan Dollu <madhusudhan.dollu@gmail.com>
2015-05-18 21:56:15 +0530

    added sample layout

--- app/views/app/index.blade.php
+++ app/views/app/index.blade.php
@@ -7,9 +7,24 @@ Vave
 
 @section('body')
 
-    <h1 class="text-center">Welcome to Vave</h1>
-    <h2>{{ Auth::user()->full_name }}</h2>
-
-    <a href="/logout">Logout</a>
+<div class="">
+    <header class="header">
+        <nav class="navbar navbar-static-top">
+            <h1 class="pull-left" style="margin-left:50px">Vave</h1>
+            <div class="pull-right" style="margin:30px 30px 0 0">
+            {{ Auth::user()->full_name }}
+            <a href="/logout" style="margin-left:20px">Logout</a>
+            </div>
+        </nav>
+    </header>
+    <nav class="container sidebar">
+        sidebar
+    </nav>
+    <div class="content-wrapper">
+        <div class="container">
+            content
+        </div>
+    </div>
+</div>
 
 @stop
--- app/views/app/layout.blade.php
+++ app/views/app/layout.blade.php
@@ -7,7 +7,7 @@
 <!--    <link rel="shortcut icon" href="{{ URL::asset('img/fav.png') }}"/>-->
     {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css') }}
     {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css')}}
-<!--    {{ HTML::style('css/style.css') }}-->
+    {{ HTML::style('css/style.css') }}
     {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') }}
 </head>
     
