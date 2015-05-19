0e684359a7affd616c43aaa7f9c021e6bae66810
Madhusudhan Dollu <madhusudhan.dollu@gmail.com>
2015-05-19 23:24:42 +0530

    added basic bootstrap template

--- app/views/app/layout.blade.php
+++ app/views/app/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="chrome=1">
     <title>@yield('title')</title>
 <!--    <link rel="shortcut icon" href="{{ URL::asset('img/fav.png') }}"/>-->
-    {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css') }}
+    {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css') }}
     {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css')}}
     {{ HTML::style('css/style.css') }}
     {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') }}
@@ -17,7 +17,9 @@
         ...
     @show
     
-    {{ HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js') }}
+    
+    {{ HTML::script('js/script.js') }}
+    {{ HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js') }}
     
     <!--[if IE]><script type="text/javascript" src="excanvas.js"></script><![endif]-->
     
